9b11e981c56090f5443c350cb55394cbc89ad3be
taktakgyun <vbmrkk@naver.com>
2022-09-21 17:59:17 +0900

    split vertical

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div class='right-box'>오른쪽1</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
