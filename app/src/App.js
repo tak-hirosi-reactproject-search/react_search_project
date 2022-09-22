cef2757714e37986abafa57c705cf958e9ea9904
Jeung-YUN <74219231+Jeung-YUN@users.noreply.github.com>
2022-09-22 09:54:12 +0900

    testgit
    한글로해본다

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload12312이거네312312.
+          Edit <code>src/App.js</code> and save to reload12312이거네네네네네.
         </p>
         <a
           className="App-link"
