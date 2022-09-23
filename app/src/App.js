9455e4b13e42821d9b58a1281a1a480ec8f414af
taktakgyun <vbmrkk@naver.com>
2022-09-23 13:52:48 +0900

    Revert "split vertical"
    This reverts commit 9b11e981c56090f5443c350cb55394cbc89ad3be.

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div class='right-box'>오른쪽1</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
