b18e73ded404e3e099f0def189a81007377bcd09
Jeung-YUN <74219231+Jeung-YUN@users.noreply.github.com>
2022-09-23 15:17:26 +0900

    2022.09.23.3pm
    흰배경, 옵션 추가 작업 진행중

--- app/src/App.js
+++ app/src/App.js
@@ -5,10 +5,33 @@ function App() {
   return (
     <div className="App">
       <div class='right-box'>오른쪽1</div>
+      <li><a href="#"> 시간시간 시간선택 UI</a></li>
+      <li><a href="#"> 종료시간 시간선택 UI</a></li>
+
+      <a href="#single">종료시간 시간선택 UI</a>
+
+
+
+
+
+
+
+      
+      
+      
+
+
+
+
+
+
+
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to 뭐한거야.
+          Edit <code>src/App.js</code> and save to Mola555555555.
         </p>
         <a
           className="App-link"
