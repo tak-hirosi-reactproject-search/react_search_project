68521f3e5cc9e20bf3397eb74f9c472beaa45971
taktakgyun <vbmrkk@naver.com>
2022-09-26 15:57:39 +0900

    react < - django (test sucess)

--- app/src/App.js
+++ app/src/App.js
@@ -1,51 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
+
 import MultiSelect from  'react-multiple-select-dropdown-lite';
 import  'react-multiple-select-dropdown-lite/dist/index.css';
 
-function App() {
-  const [value, setvalue] = useState('')
 
-  const  handleOnchange  =  val  => {
-    setvalue(val)
+
+
+class App extends Component {
+  state = {
+    videos: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/');
+      const videos = await res.json();
+      this.setState({
+        videos
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
-  const  options  = [
-    { label:  'Option 1', value:  'option_1'  },
-    { label:  'Option 2', value:  'option_2'  },
-    { label:  'Option 3', value:  'option_3'  },
-    { label:  'Option 4', value:  'option_4'  },
-  ]
+  render() {
+    const [value, setvalue] = useState('');
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to Mola555555555.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className='select-menu'>
-        <div className='video-menu'>
-          <MultiSelect
-            onChange={handleOnchange}
-            options={options}
-            name="영상선택"
-          />
+    const handleOnchange  =  val  => {
+      setvalue(val)
+    };
+
+    const  options  = this.state.videos.reduce((newObj, obj) =>{
+      newObj[obj.id] = obj.name;
+      return newObj;
+    });
+    console.log(options);
+
+    return (
+      <div>
+        <div className='select-menu'>
+          <div className='video-menu'>
+            <MultiSelect
+              onChange={handleOnchange}
+              options={options}
+              name="영상선택"
+            />
+          </div>
         </div>
+        
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
